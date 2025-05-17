b5cc390cf88615cb5d765e213236bcfd21a5e9c7
Sidra Jahangir <sidrajahangir93@gmail.com>
2025-05-17 20:49:57 +0530

    Added documents

--- js/downloads.js
+++ js/downloads.js
@@ -15,3 +15,11 @@ function filterDownloads() {
         }
     });
 }
+
+const currentPath = window.location.pathname.split("/").pop();
+document.querySelectorAll("nav ul li a").forEach(link => {
+  if (link.getAttribute("href") === currentPath) {
+    link.classList.add("active");
+  }
+});
+
