d5e7e15615d2cf82c53682ee1d077ebdab650e4f
Sidra Jahangir <sidrajahangir93@gmail.com>
2025-05-17 21:12:01 +0530

    fixed transiton issue

--- js/help_taxpayer.js
+++ js/help_taxpayer.js
@@ -3,13 +3,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
     faqTitles.forEach(button => {
         button.addEventListener("click", function() {
+            this.classList.toggle("active");
             const faqDetail = this.nextElementSibling;
-            if (faqDetail.style.display === "block") {
-                faqDetail.style.display = "none";
-            } else {
-                faqDetail.style.display = "block";
-            }
+            faqDetail.classList.toggle("show");
         });
     });
 });
+
+
+const currentPath = window.location.pathname.split("/").pop();
+document.querySelectorAll("nav ul li a").forEach(link => {
+  if (link.getAttribute("href") === currentPath) {
+    link.classList.add("active");
+  }
+});
+
 
