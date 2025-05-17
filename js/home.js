09441f0accd33f22e48de00fe2ff3d8efcfca5c2
Sidra Jahangir <sidrajahangir93@gmail.com>
2025-05-17 15:14:45 +0530

    fixed chart issue

--- js/home.js
+++ js/home.js
@@ -22,3 +22,10 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+        const answer = button.nextElementSibling;
+        answer.classList.toggle('hidden');
+    });
+});
